dc2e77e9a22b2e5c25a10054ecd78d1fa0558835
kalpesh05 <kalpeswhbhad57@gail.com>
2019-02-24 06:37:36 -0800

    github login using new pakcage and craeting jwt token from profile

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,23 @@
 import express from 'express';
 import dotenv from "dotenv";
+// import passport from "passport";
+import axios from "axios";
+import cors from "cors";
+import bodyParser from "body-parser";
+import jwt from "jsonwebtoken";
+
+axios.defaults.headers.post['Accept'] = 'application/json';
+
 dotenv.config();
 
+
+
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
 
 app.get('/api/me', (req, res) => {
   
@@ -13,18 +28,80 @@ app.get('/api/me', (req, res) => {
   });
 });
 
-// call back url
-app.get(`/user/signin/callback`, (req, res,next) => {
+// // call back url
+// app.get(`/user/signin/callback`, (req, res,next) => {
 
-  // query parameter comes from github 
-  const { query } = req;
+//   // query parameter comes from github 
+//   const { query } = req;
 
-  // extract code from query paramter
-  const { code } = query;
+//   // extract code from query paramter
+//   const { code } = query;
 
-  // showing log for code which we get 
-  console.log(code);
+//   // showing log for code which we get 
+//   console.log(code);
   
+// });
+
+// api to create github token 
+app.post('/api/github/token',(req,res,next) => {
+
+    // getting code from callback
+    var code = req.body.code;
+
+    // API for getting token from github
+    return axios.post('https://github.com/login/oauth/access_token', {
+        client_id:	process.env.GITHUB_CLIENT,
+        client_secret:	process.env.GITHUB_SECRET,
+        code: code
+    })
+    .then((response) => {
+      // store token in varioable
+      let token = response.data.access_token;
+
+      // set tone in header for fetching user 
+      axios.defaults.headers.common['Authorization'] = 'token ' + token;
+
+      // api for getting profile data
+      return axios.get('https://api.github.com/user').then(resp => {
+        // storung user prifle for creating access token
+        let user = resp.data;
+
+        // genrating jwt token with expired in 24 hour and sending to front
+        jwt.sign(user, "curran", { expiresIn: 60 * 60 * 24 }, (err, token) => {
+          res.json({
+            token
+          });
+        });
+
+      }).catch(err => {
+        res.send(err);
+      });      
+    })
+    .catch((error) => {
+      res.send(error)
+    });
+
 });
+
+// verifying token
+function verifyToken(req, res, next) {
+  // getting auth header
+  const bearerHeader = req.headers["authorization"];
+
+  //check if tokeni there or not
+  if (typeof bearerHeader !== "undefined") {
+    //split at the space
+    const bearer = bearerHeader.split(" ");
+    // egt token from splited array
+    const bearerToken = bearer[1];
+    // set token
+    req.token = bearerToken;
+    // proceed next
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
+
 
 app.listen(4000);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import dotenv from "dotenv";
 import './App.css';
+import GitHubLogin from 'react-github-login';
+import axios from "axios";
+
 dotenv.config()
 
 
@@ -8,11 +11,34 @@ dotenv.config()
 const App = () => {
   const [user, setUser] = useState(null);
 
+
   useEffect(() => {
     fetch('/api/me')
       .then(response => response.json())
       .then(setUser);
   }, []);
+
+
+  // getting success data after github login
+  const onSuccess = response => {
+    console.log(response)
+   axios.post('http://localhost:4000/api/github/token', {
+    code:response.code
+    })
+    .then((response) => {
+      // console token 
+      console.log(response.data);
+     
+    })
+    .catch((error) => {
+      console.error(error)
+    });
+  
+  };
+
+  // failure data in case login fails
+  const onFailure = response => console.error(response);
+  
 
   return (
     <div className="App">
@@ -24,12 +50,18 @@ const App = () => {
                 ? 'Authenticated id: ' + user.id
                 : 'Not authenticated.'}
             </p>
-            <button>{user.authenticated ? 'Sign out' : 'Sign in'}</button>
+            <button >{user.authenticated ? 'Sign out' : 'Sign in'}</button>
           </>
         ) : null}
         <br/>
         {/*  for test github login functionality  */} 
-        <a href={`https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_CLIENT}`} alt="#">github</a>
+        {/* <a href={`https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_CLIENT}`} alt="#">github</a> */}
+        {/*  use github login package for proper view and getting data back coded in back end  */} 
+        <br/>
+        <GitHubLogin clientId={process.env.REACT_APP_GITHUB_CLIENT}
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          redirectUri="http://localhost:3000/github/callback"/>
       </header>
     </div>
   );
