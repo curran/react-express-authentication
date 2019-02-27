2bf681dd6b185a8672f75917d7ee549fc716acde
curran <curran.kelleher@gmail.com>
2019-02-27 23:41:38 +0530

    Auto-format code using Prettier, for easy readability

--- backend/index.js
+++ backend/index.js
@@ -1,157 +1,147 @@
 import express from 'express';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 // import passport from "passport";
-import cors from "cors";
-import bodyParser from "body-parser";
-import jwt from "jsonwebtoken";
-import fetch from "node-fetch";
-
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import jwt from 'jsonwebtoken';
+import fetch from 'node-fetch';
 
 // axios.defaults.headers.post['Accept'] = 'application/json';
 
 dotenv.config();
 
-
-
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/api/get/data",(re,res) => {
-  let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGVzdCIsImlkIjoiZGF0YS5pZCIsImlhdCI6MTU1MTI4NTE2NSwiZXhwIjoxNTUxMzcxNTY1fQ.e-0SBtbKYz5K7QAK9hDtfg0QFgf8cqsF365lco_0sm8';
-  let details = jwt.verify(token,process.env.SECERT,{ignoreExpiration: true});
+app.get('/api/get/data', (re, res) => {
+  let token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGVzdCIsImlkIjoiZGF0YS5pZCIsImlhdCI6MTU1MTI4NTE2NSwiZXhwIjoxNTUxMzcxNTY1fQ.e-0SBtbKYz5K7QAK9hDtfg0QFgf8cqsF365lco_0sm8';
+  let details = jwt.verify(token, process.env.SECERT, {ignoreExpiration: true});
 
   res.json({
-    data: details
+    data: details,
   });
 });
 
-app.get('/api/me', verifyToken,(req, res) => {
-
+app.get('/api/me', verifyToken, (req, res) => {
   // decode jwt to get data from it
-  const details = jwt.verify(req.token,process.env.SECERT,{ignoreExpiration: true});
-  
+  const details = jwt.verify(req.token, process.env.SECERT, {
+    ignoreExpiration: true,
+  });
 
   // TODO return data about the currently authenticated user.
   res.json({
     authenticated: true,
     id: details.id,
-    username: details.user
+    username: details.user,
   });
 });
 
 // // call back url
 // app.get(`/user/signin/callback`, (req, res,next) => {
 
-//   // query parameter comes from github 
+//   // query parameter comes from github
 //   const { query } = req;
 
 //   // extract code from query paramter
 //   const { code } = query;
 
-//   // showing log for code which we get 
+//   // showing log for code which we get
 //   console.log(code);
-  
+
 // });
 
-// api to create github token 
-app.post('/api/github/token',cors(),(req,res,next) => {
+// api to create github token
+app.post('/api/github/token', cors(), (req, res, next) => {
+  // getting code from callback
+  const code = req.body.code;
 
+  // setting data for sending to github
+  const data = {
+    client_id: process.env.GITHUB_CLIENT,
+    client_secret: process.env.GITHUB_SECRET,
+    code: code,
+  };
 
+  // api calling for fetching github user profile
+  return fetch('https://github.com/login/oauth/access_token', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then(response => response.json())
+    .then(data => {
+      // store token in variaable
+      const accessToken = data.access_token;
 
-    // getting code from callback
-      const code = req.body.code;
-
-      // setting data for sending to github
-      const data = {
-        client_id:	process.env.GITHUB_CLIENT,
-        client_secret:	process.env.GITHUB_SECRET,
-        code: code
-      };
-
-      // api calling for fetching github user profile  
-    return fetch('https://github.com/login/oauth/access_token', {
-          method: 'POST',
-          mode: "cors",
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(data)
-        })
-        .then((response) => response.json())
-        .then((data) => {
-              // store token in variaable
-              const accessToken = data.access_token;
-
-            // set token in header for fetching user 
-            // api for getting profile data
-              return fetch('https://api.github.com/user',{
-                method: 'GET',
-                mode: "cors",
-                headers: {
-                  'Accept': 'application/json',
-                  'Content-Type': 'application/json',
-                  'Authorization': `token ${accessToken}`
-                }
-              })
-              .then((response) => response.json())
-              .then((data) => {
-                  // storing user profile for creating access token
-                  const user = {
-                    user: data.login,
-                    id: data.id
-                  };
-
-
-                  // generating jwt token with expired in 24 hour and sending to front
-                  jwt.sign(user, process.env.SECERT, { expiresIn: 60 * 60 * 24 }, (err, token) => {
-                    res.json({
-                      token
-                    });
-                    console.log("l"+token);
-                  });
-
-            });
+      // set token in header for fetching user
+      // api for getting profile data
+      return fetch('https://api.github.com/user', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: `token ${accessToken}`,
+        },
       })
-      .catch((error) => {
-        // sending error if it occurs
-        res.send(error)
-      });
+        .then(response => response.json())
+        .then(data => {
+          // storing user profile for creating access token
+          const user = {
+            user: data.login,
+            id: data.id,
+          };
 
-
+          // generating jwt token with expired in 24 hour and sending to front
+          jwt.sign(
+            user,
+            process.env.SECERT,
+            {expiresIn: 60 * 60 * 24},
+            (err, token) => {
+              res.json({
+                token,
+              });
+              console.log('l' + token);
+            },
+          );
+        });
+    })
+    .catch(error => {
+      // sending error if it occurs
+      res.send(error);
+    });
 });
 
 // verifying token
 function verifyToken(req, res, next) {
   // getting auth header
-  const bearerHeader = req.headers["authorization"];
-  
-  
+  const bearerHeader = req.headers['authorization'];
+
   //check if token is there or not
   if (bearerHeader) {
     //split at the space
-    const bearer = bearerHeader.split(" ");
+    const bearer = bearerHeader.split(' ');
     // get token from splited array
     const bearerToken = bearer[1];
 
     // set token
     req.token = bearerToken;
-     
-    if(bearerToken === "null"){
 
-        res.status(401).send("token not valid!");         
-    }else{
+    if (bearerToken === 'null') {
+      res.status(401).send('token not valid!');
+    } else {
       // proceed next
       next();
     }
-
   } else {
-    res.status(401).send("User not authenticated!");
+    res.status(401).send('User not authenticated!');
   }
 }
-
-
-
 
 app.listen(4000);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,109 +1,92 @@
 import React, {useState, useEffect} from 'react';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 import './App.css';
-import { withCookies, Cookies } from 'react-cookie'
+import {withCookies, Cookies} from 'react-cookie';
 // import GitHubLogin from 'react-github-login';
 
-
-dotenv.config()
+dotenv.config();
 
 const cookies = new Cookies();
 
+const App = props => {
+  const {cookies} = props;
+  let accessToken = cookies.get('token') || null;
 
-const App = (props) => {
-  const { cookies } = props; 
-  let accessToken =  cookies.get('token') ||  null;
-  
   // alert(accessToken);
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
   const [token, setToken] = useState(accessToken);
 
-
   useEffect(() => {
-
-
-
     //event listener for getting data back from popup
 
-    window.addEventListener('message', function(event){
-
+    window.addEventListener('message', function(event) {
       // checking access code available or not
 
-        if(!event.data.includes("setImmediate")){
-      
-          // storing code 
-          // let code =
-          //  if(event.data.includes("setImmediate")){
-          //    alert();
-          //  }
+      if (!event.data.includes('setImmediate')) {
+        // storing code
+        // let code =
+        //  if(event.data.includes("setImmediate")){
+        //    alert();
+        //  }
 
+        // create data variable to send in api
 
-          // create data variable to send in api
+        let data = {
+          code: event.data,
+        };
 
-          let data = {
-            code:event.data
-          };
+        // api for getting jwt token from backend api
 
-          // api for getting jwt token from backend api
+        fetch('/api/github/token', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        })
+          .then(response => response.json())
+          .then(function(data) {
+            // checking data contains token or not
+            if (data.token) {
+              console.log(data.token);
+              // setting token
+              setToken(data.token);
+              cookies.set('token', data.token, {path: '/'});
 
-          fetch('/api/github/token', {
-                method: 'POST',
+              // try to call api again after getting token
+              fetch('/api/me', {
+                method: 'GET',
                 headers: {
-                  'Accept': 'application/json',
-                  'Content-Type': 'application/json'
+                  Authorization: `token ${token}`,
                 },
-                body: JSON.stringify(data)
-            }).then(response => response.json())
-            .then(function(data) {
-
-                // checking data contains token or not
-                if(data.token){
-                  console.log(data.token);
-                  // setting token
-                  setToken(data.token)
-                  cookies.set('token', data.token, { path: '/' });
-
-
-                  // try to call api again after getting token
-                  fetch('/api/me',{ 
-                          method: "GET",
-                          headers: {
-                                    'Authorization': `token ${token}`
-                                  }
-                  })
-                  .then(response => response.json())
-                  .then(setUser)
-                  .catch(error => {
-                      setError(error);
-                      console.log('error============:', error)
-                    });
-      
-                }
-
-            });
-          
-        }
-    }); 
-
+              })
+                .then(response => response.json())
+                .then(setUser)
+                .catch(error => {
+                  setError(error);
+                  console.log('error============:', error);
+                });
+            }
+          });
+      }
+    });
 
     // calling api to check token authentication wokring or not
-      fetch('/api/me',{ 
-                          method: "GET",
-                          headers: {
-                                    'Authorization': `token ${token}`
-                                  }
-                  })
-        .then(response => response.json())
-        .then(setUser)
-        .catch(error => {
-          setError(error);
-          console.log('error============:', error)
-        });
-
-
+    fetch('/api/me', {
+      method: 'GET',
+      headers: {
+        Authorization: `token ${token}`,
+      },
+    })
+      .then(response => response.json())
+      .then(setUser)
+      .catch(error => {
+        setError(error);
+        console.log('error============:', error);
+      });
   }, []);
-
 
   // // getting success data after github login
   // const onSuccess = response => {
@@ -112,73 +95,87 @@ const App = (props) => {
   // //   code:response.code
   // //   })
   // //   .then((response) => {
-  // //     // console token 
+  // //     // console token
   // //     console.log(response.data);
-     
+
   // //   })
   // //   .catch((error) => {
   // //     console.error(error)
   // //   });
-  
+
   // };
 
   // failure data in case login fails
   // const onFailure = response => console.error(response);
 
-
   // function for github login
-  const handleLogin = (url,id) => {
-
-    const op = PopupCenter(url, id,600,1000);
-
+  const handleLogin = (url, id) => {
+    const op = PopupCenter(url, id, 600, 1000);
   };
 
   // function for handle user logout
-  const handleLogout = (props) => {
-      // const { cookies } = this.props;
+  const handleLogout = props => {
+    // const { cookies } = this.props;
 
-      let accessToken = cookies.remove('token');
-      setToken(accessToken);
-      setUser(null);
+    let accessToken = cookies.remove('token');
+    setToken(accessToken);
+    setUser(null);
   };
 
+  // function to open popup window at center
 
- // function to open popup window at center
-
-  const PopupCenter = (url, title, w, h)  => {
+  const PopupCenter = (url, title, w, h) => {
     // Fixes dual-screen position                         Most browsers      Firefox
-    const dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : window.screenX;
-    const dualScreenTop = window.screenTop != undefined ? window.screenTop : window.screenY;
+    const dualScreenLeft =
+      window.screenLeft != undefined ? window.screenLeft : window.screenX;
+    const dualScreenTop =
+      window.screenTop != undefined ? window.screenTop : window.screenY;
 
-    const width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : window.screen.width;
-    const height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : window.screen.height;
+    const width = window.innerWidth
+      ? window.innerWidth
+      : document.documentElement.clientWidth
+      ? document.documentElement.clientWidth
+      : window.screen.width;
+    const height = window.innerHeight
+      ? window.innerHeight
+      : document.documentElement.clientHeight
+      ? document.documentElement.clientHeight
+      : window.screen.height;
 
     const systemZoom = width / window.screen.availWidth;
-    const left = (width - w) / 2 / systemZoom + dualScreenLeft
-    const top = (height - h) / 2 / systemZoom + dualScreenTop
-    const newWindow = window.open(url, title, 'scrollbars=yes, width=' + w / systemZoom + ', height=' + h / systemZoom + ', top=' + top + ', left=' + left);
+    const left = (width - w) / 2 / systemZoom + dualScreenLeft;
+    const top = (height - h) / 2 / systemZoom + dualScreenTop;
+    const newWindow = window.open(
+      url,
+      title,
+      'scrollbars=yes, width=' +
+        w / systemZoom +
+        ', height=' +
+        h / systemZoom +
+        ', top=' +
+        top +
+        ', left=' +
+        left,
+    );
 
     // Puts focus on the newWindow
     if (window.focus) newWindow.focus();
     const interval = setInterval(() => {
-      
-      if(newWindow.location.pathname === 'blank'){
-        return
+      if (newWindow.location.pathname === 'blank') {
+        return;
       }
 
-      if(newWindow.location.search){
-         const search = newWindow.location.search.split('=');
+      if (newWindow.location.search) {
+        const search = newWindow.location.search.split('=');
         //  console.log("h"+search)
-         window.postMessage(search[1]);     
+        window.postMessage(search[1]);
 
-         clearInterval(interval);
+        clearInterval(interval);
       }
 
       newWindow.close();
     }, 500);
-
-};
-
+  };
 
   return (
     <div className="App">
@@ -192,12 +189,30 @@ const App = (props) => {
             </p>
             <button onClick={() => handleLogout()}> Sign out</button>
           </>
-        ) : <button onClick={() => handleLogin(`https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_CLIENT}`,'github-oauth-authorize')}>{'Sign in'}</button>}
-        <br/>
-        {/*  for test github login functionality  */} 
-        <a href={`https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_CLIENT}`} alt="#">github</a>
-        {/*  use github login package for proper view and getting data back coded in back end  */} 
-        <br/>
+        ) : (
+          <button
+            onClick={() =>
+              handleLogin(
+                `https://github.com/login/oauth/authorize?client_id=${
+                  process.env.REACT_APP_GITHUB_CLIENT
+                }`,
+                'github-oauth-authorize',
+              )
+            }>
+            {'Sign in'}
+          </button>
+        )}
+        <br />
+        {/*  for test github login functionality  */}
+        <a
+          href={`https://github.com/login/oauth/authorize?client_id=${
+            process.env.REACT_APP_GITHUB_CLIENT
+          }`}
+          alt="#">
+          github
+        </a>
+        {/*  use github login package for proper view and getting data back coded in back end  */}
+        <br />
         {/* <GitHubLogin clientId={process.env.REACT_APP_GITHUB_CLIENT}
           onSuccess={onSuccess}
           onFailure={onFailure}
